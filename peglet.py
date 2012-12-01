f37d884e942044f9e71b2c5fabdd8f46d717c84c
Darius Bacon <darius@wry.me>
2012-12-01 12:57:47 -0500

    'not parts' can't happen

--- peglet.py
+++ peglet.py
@@ -56,9 +56,7 @@ def Parser(grammar, **actions):
     """
     parts = re.split(' ('+_identifier+') += ',
                      ' '+re.sub(r'\s', ' ', grammar))
-    if not parts:
-        raise BadGrammar("No grammar")
-    if parts[0].strip():
+    if len(parts) <= 1 or parts[0].strip():
         raise BadGrammar("Missing left hand side", parts[0])
     if len(set(parts[1::2])) != len(parts[1::2]):
         raise BadGrammar("Multiply-defined rule(s)", grammar)
