f639ade8a96fd7265d93aa0b5a094a81814a93ee
Darius Bacon <darius@wry.me>
2012-12-06 18:27:01 -0500

    add infix example with left-to-right associating ops

--- examples/infix.py
+++ examples/infix.py
@@ -1,36 +1,85 @@
-from operator import *
-from peglet import *
+"""
+Parsing infix expressions that can associate left-to-right, like
+   5 - 3 - 1 (meaning: (5 - 3) - 1)
+is tricky, I'm afraid, because here's the natural way to express
+it in a grammar, which doesn't work:
+
+  expr = expr (-) factor   hug
+       | expr
+  factor = (\d+)
+
+This dies with a stack overflow because of the recursion
+  expr = expr ...
+
+By moving the recursive call over to the right, we can fix this,
+at the cost of producing the wrong parse tree:
+
+  expr = factor (-) expr   hug
+       | factor
+  factor = (\d+)
+
+This would parse it like 5-(3-1) instead. So, in the code below, we
+first build the wrong tree this way, but tagging the parts that are
+wrong (with misassociated()); then we fix them up in reassociate().
+
+It's a hack. I don't see a better way without making the parsing
+library more complex. (Suggestions solicited.)
+"""
+
+from peglet import Parser, hug
+
+def misassociated(x, op, y):
+    return 'assoc', x, op, y
 
 def reassociate(exp):
-    if len(exp) == 3:
-        x, op1, y = exp
-        if len(y) == 3:
-            u, op2, v = y
-            # x - (u + v) -> (x - u) + v
-            return reassociate(((x, op1, u), op2, v))
-        else:
-            assert len(y) == 1
-            return (x, op1, y[0])
-    else:
-        assert len(exp) == 1
-        return exp[0]
+    if isinstance(exp, tuple) and exp[0] == 'assoc':
+        _, t, op1, rhs = exp
+        while isinstance(rhs, tuple) and rhs[0] == 'assoc':
+            _, u, op2, v = rhs
+            # t <op1> (u <op2> v) ==> (t <op1> u) <op2> v
+            # e.g.    t - (u + v) ==> (t - u) + v
+            t, op1, rhs = (t, op1, u), op2, v
+        exp = t, op1, rhs
+    return exp
 
 calc = Parser(r"""
-top    = _ exp $
+top    = _ exp0 $
 
-exp    = exp0                   reassociate
-exp0   = factor ([+-]) _ exp0   hug
-       | factor                 hug              
+exp0   = exp0a                  reassociate
+exp0a  = exp1 ([+-]) _ exp0a    misassociated
+       | exp1
+
+exp1   = exp1a                  reassociate
+exp1a  = exp2 ([*/%]) _ exp1a   misassociated
+       | exp2
+
+exp2   = factor (\^) _ exp2     hug
+       | factor
 
 factor = (-) _ factor           hug
-       | ([\d+])                int
-       | \( exp \)
+       | ([\d+]) _              int
+       | \( exp0 \)
 
 _      = \s*
 """, 
               int=int,
               hug=hug,
+              misassociated=misassociated,
               reassociate=reassociate)
 
+# So, operators are left-associative except ^ which associates to the right.
+
+## calc('3')
+#. (3,)
+## calc('3-1')
+#. ((3, '-', 1),)
+## calc('3-2-1')
+#. (((3, '-', 2), '-', 1),)
+## calc('3/1/1')
+#. (((3, '/', 1), '/', 1),)
 ## calc('3-1-(1-2)')
 #. (((3, '-', 1), '-', (1, '-', 2)),)
+## calc('2 - 4/5')
+#. ((2, '-', (4, '/', 5)),)
+## calc('2^3^4')
+#. ((2, '^', (3, '^', 4)),)
