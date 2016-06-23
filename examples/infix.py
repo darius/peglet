1d509530bcf468a102e998c838343f00daaaf256
Darius Bacon <darius@wry.me>
2016-06-22 23:29:23 -0400

    a less embarrassing hack for left-to-right associativity

--- examples/infix.py
+++ examples/infix.py
@@ -1,76 +1,62 @@
-r"""
-Parsing infix expressions that can associate left-to-right, like
+"""
+Parsing infix expressions that associate left-to-right, like
    5 - 3 - 1 (meaning: (5 - 3) - 1)
-is tricky, I'm afraid, because here's the natural way to express
-it in a grammar, which doesn't work:
+is a little tricky, because the natural way to express it in a
+grammar, like this, doesn't work:
 
   expr = expr (-) term   hug
        | expr
   term = (\d+)
 
-This dies with a stack overflow because of the recursion
+This dies with a stack overflow because the recursion
   expr = expr ...
+precedes any base case.
 
-By moving the recursive call over to the right, we can fix this,
-at the cost of producing the wrong parse tree:
+Moving the recursive call over to the right would fix the problem,
+at the cost of producing the wrong parse tree, a tree like 5-(3-1):
 
   expr = term (-) expr   hug
        | term
-  term = (\d+)
 
-This would parse it like 5-(3-1) instead. So, in the code below, we
-parse the wrong way, but fix up the tree as we build it, with 
-reassociate(). Shelter protects subtrees from this reassociation.
+So the code below instead takes the form
 
-It's a hack. I don't see a better way without making peglet more
-complex. (Ideas solicited.) (My parson package handles this nicely.)
+  expr = term ops
+  ops  = (-) term ops
+       | 
+
+plus semantic actions that juggle the values into place. (My
+other parsing library, Parson, can manage without the juggling.)
 """
 
-from peglet import Parser, hug
+from peglet import Parser, OneResult, hug
 
-class Shelter:
-    def __init__(self, exp):
-        self.sheltered = expose(exp)
+def apply(a, f):   return f(a)
+def rhs(op, b):    return lambda a: (a, op, b)
+def compose(f, g): return lambda a: g(f(a))
+def identity():    return lambda a: a
 
-def expose(exp):
-    return exp.sheltered if isinstance(exp, Shelter) else exp
+g = r"""
+top   =  _ exp0 $
 
-def reassociate(x, op, y):
-    "x-(u-v) => (x-u)-v and recursively."
-    x = expose(x)
-    if isinstance(y, tuple):
-        return reassociate(x, op, y[0]), y[1], expose(y[2])
-    else:
-        return x, op, expose(y)
+exp0  =  exp1 ops0 apply 
+ops0  =  ([+-]) _ exp1 rhs ops0 compose
+      |  identity
 
-parse = Parser(r"""
-top  = _ exp0 $
-
-exp0 = exp1 ([+-]) _ exp0    reassociate
-     | exp1                  Shelter
-
-exp1 = exp2 ([*/%]) _ exp1   reassociate
-     | exp2                  Shelter
+exp1  =  exp2 ops1 apply
+ops1  =  ([*/%]) _ exp2 rhs ops1 compose
+      |  identity
 
 exp2 = term (\^) _ exp2      hug
      | term
 
-term = (-) _ exp1            hug
-     | ([\d+]) _             int
-     | \( _ exp0 \) _
+term  =  (-) _ exp1          hug
+      |  \( _ exp0 \) _
+      |  (\d+) _             int
 
-_    = \s*
-""", 
-              int=int,
-              hug=hug,
-              Shelter=Shelter,
-              reassociate=reassociate)
+_     =  \s*
+"""
 
-def calc(s):
-    exp, = parse(s)
-    return expose(exp)
-
-# So, operators are left-associative except ^ which associates to the right.
+calc = OneResult(Parser(g, int=int, **globals()))
 
 ## calc('3')
 #. 3
@@ -78,8 +64,8 @@ def calc(s):
 #. (3, '-', 1)
 ## calc('5-4-3-2-1')
 #. ((((5, '-', 4), '-', 3), '-', 2), '-', 1)
-## calc('3/1/1')
-#. ((3, '/', 1), '/', 1)
+## calc('60/5/6')
+#. ((60, '/', 5), '/', 6)
 ## calc('3-1-(1-2)')
 #. ((3, '-', 1), '-', (1, '-', 2))
 ## calc('2 - 4/5')
